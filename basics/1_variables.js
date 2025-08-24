c357be194938ad3582ba870d96e1dbbb7df95f06
Nikhil-654 <rawatnikhil654@gmail.com>
2025-08-24 10:19:52 +0530

    Initial commit

---
+++ basics/1_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553;
+let accountEmail = "nikhil@gmail.com";
+var accountPassword = "12345";
+accountCity = "Dehradun";
+
+// accountId = 2;
+accountEmail = "Shishir@gmail.com";
+accountPassword = "232323";
+accountCity = "Bengaluru";
+let accountState
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
